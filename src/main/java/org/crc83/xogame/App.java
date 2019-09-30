18762449ed1bd60dce62df8d3dd2efd713a4f3c7
Serhii Belei <serhii.belei@weather.com>
2019-09-30 20:51:14 +0300

    Initial commit

---
+++ src/main/java/org/crc83/xogame/App.java
@@ -0,0 +1,13 @@
+package org.crc83.xogame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
