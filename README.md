edd8ba8a08ffadb653a8afd48eb8139fb0e63673
Serhii Belei <serhii.belei@weather.com>
2019-09-30 21:11:44 +0300

    Sample app works.

---
+++ README.md
@@ -0,0 +1,7 @@
+Tutorial videos
+
+https://youtu.be/48rVgdq0mFA?list=PL4h6ypqTi3RTiTuAQFKE6xwflnPKyFuPp
+
+https://youtu.be/_SwZ2_rWiB4?list=PL4h6ypqTi3RTiTuAQFKE6xwflnPKyFuPp
+
+https://youtu.be/6zNM2o6didU
--- src/main/java/org/crc83/xogame/App.java
+++ src/main/java/org/crc83/xogame/App.java
@@ -1,13 +1,22 @@
 package org.crc83.xogame;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
